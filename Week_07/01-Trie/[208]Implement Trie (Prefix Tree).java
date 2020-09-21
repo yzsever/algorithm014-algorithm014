2ae5d9a7aff1643d6bf183974fa3d82334d8947a
yzs0331 <yaozs@magima.com.cn>
2020-09-21 17:57:05 +0800

    添加代码

---
+++ Week_07/01-Trie/[208]Implement Trie (Prefix Tree).java
@@ -0,0 +1,80 @@
+//Implement a trie with insert, search, and startsWith methods. 
+//
+// Example: 
+//
+// 
+//Trie trie = new Trie();
+//
+//trie.insert("apple");
+//trie.search("apple");   // returns true
+//trie.search("app");     // returns false
+//trie.startsWith("app"); // returns true
+//trie.insert("app");   
+//trie.search("app");     // returns true
+// 
+//
+// Note: 
+//
+// 
+// You may assume that all inputs are consist of lowercase letters a-z. 
+// All inputs are guaranteed to be non-empty strings. 
+// 
+// Related Topics Design Trie 
+// 👍 3670 👎 56
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Trie {
+    private Trie[] next;
+    private boolean isEnd;
+    /** Initialize your data structure here. */
+    public Trie() {
+        next = new Trie[26];
+        isEnd = false;
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        if (word == null || word.length() == 0) return;
+        Trie curr = this;
+        for (int i = 0; i < word.length(); i++) {
+            int n = word.charAt(i) - 'a';
+            if (curr.next[n] == null) {
+                curr.next[n] = new Trie();
+            }
+            curr = curr.next[n];
+        }
+        curr.isEnd = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        Trie node = searchPrefix(prefix);
+        return node != null;
+    }
+
+    public Trie searchPrefix(String prefix) {
+        if (prefix == null || prefix.length() == 0) return this;
+        Trie curr = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            curr = curr.next[prefix.charAt(i) - 'a'];
+            if (curr == null) return null;
+        }
+        return curr;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
